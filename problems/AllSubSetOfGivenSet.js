43f464bfc1fd044fcd15881239a9ddbd405c31d5
Arun Kumar Giri <atulgiri649@gmail.com>
2025-03-29 21:31:27 +0530

    Update AllSubSetOfGivenSet.js

--- problems/AllSubSetOfGivenSet.js
+++ problems/AllSubSetOfGivenSet.js
@@ -1,7 +1,18 @@
-const getAllSubset = arr => 
-  arr.reduce((subsets, value) =>
-    subsets.concat(subsets.map(set => [value, ...set]))
-  , [[]]
-);
+// const getAllSubset = arr => 
+//   arr.reduce((subsets, value) =>
+//     subsets.concat(subsets.map(set => [value, ...set]))
+//   , [[]]
+// );
 
-console.log(getAllSubsets([1,2,3])); // [[], [1], [2], [2, 1], [3], [3, 1], [3, 2], [3, 2, 1]]
+// console.log(getAllSubsets([1,2,3])); // [[], [1], [2], [2, 1], [3], [3, 1], [3, 2], [3, 2, 1]]
+
+
+
+const generateSubsets = arr => {
+  return arr.reduce((subsets, currentValue) => {
+    return subsets.concat(subsets.map(subset => [...subset, currentValue]));
+  }, [[]]);
+};
+
+console.log(generateSubsets([1, 2, 3]));
+
